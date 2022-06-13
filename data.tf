b3034d0c88a35d7ad3f49ce120a5195f4038573c
Uchinda Padmaperuma <uchinda@sph.com.sg>
2022-06-13 18:41:30 +0800

    remove plain test fetch

--- data.tf
+++ data.tf
@@ -6,7 +6,4 @@ data "aws_caller_identity" "current" {
 
 data "http" "ascp_manifest" {
   url = "https://raw.githubusercontent.com/aws/secrets-store-csi-driver-provider-aws/main/deployment/aws-provider-installer.yaml"
-  request_headers = {
-    Accept = "text/plain"
-  }
 }
